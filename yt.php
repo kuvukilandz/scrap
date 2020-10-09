2b2a1f68665b45ca83c741900b2c6542a201c485
renzichwan <68050501+renzichwan@users.noreply.github.com>
2020-10-09 00:54:04 -0700

    Update yt.php

--- yt.php
+++ yt.php
@@ -1,6 +1,6 @@
 <?php
 $q = $_GET['q'];
-$ch = curl_init('http://youtube-scrape.herokuapp.com/api/search?q='.$q.'&page=1');
+$ch = curl_init('http://youtube-scrape.herokuapp.com/api/search?q=Alan Walker Force&page=1');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $upx = curl_exec($ch);
 $data = json_decode($upx,true);
