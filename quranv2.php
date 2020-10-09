32fb7874e82faaa8fe799b47389c6ec2f7235252
renzichwan <68050501+renzichwan@users.noreply.github.com>
2020-10-09 06:47:36 -0700

    Update quranv2.php

--- quranv2.php
+++ quranv2.php
@@ -1,9 +1,8 @@
 <?php
 $surah = $_GET['surah'] - 1;
-$surahr = $surah + 1;
 $ch = curl_init('https://raw.githubusercontent.com/penggguna/QuranJSON/master/quran.json');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $upx = curl_exec($ch);
 $data = json_decode($upx,true);
-print_r($data);
+echo $data[$surah]['recitation];
 ?>
