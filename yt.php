f23c2a5e4319d09a84d848eafed51b324148d0d3
renzichwan <68050501+renzichwan@users.noreply.github.com>
2020-10-09 01:01:06 -0700

    Update yt.php

--- yt.php
+++ yt.php
@@ -4,6 +4,7 @@ $ch = curl_init('http://youtube-scrape.herokuapp.com/api/search?q=Alan%20Walker%
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $upx = curl_exec($ch);
 $data = json_decode($upx,true);
-print_r($upx)."<br>";
+print_r($upx);
+echo "<br>";
  echo $upx['results']['video']['title']."|".$upx['results']['video']['url'];
 ?>
