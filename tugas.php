690b991e76448c235bbbcefbbd6752be448f556e
renzichwan <68050501+renzichwan@users.noreply.github.com>
2020-10-09 22:50:05 -0700

    Update tugas.php

--- tugas.php
+++ tugas.php
@@ -1,18 +1,13 @@
 <?php
 echo"
-*[+] TUGAS KELAS A [+] \n
------------------------- \n
-1. Phonology \n
-  - _Mencari Definisi Phonlogy_ \n
------------------------- \n
-\n
------------------------- \n
-2. Bahasa Indonesia \n
-  - _Mencari 15 Pengertin Bahasa Menurut Para Ahli_ \n
------------------------- \n
-
-
-
-
+*[+] TUGAS KELAS A [+] 
+------------------------
+1. Phonology
+  - _Mencari Definisi Phonlogy_
+------------------------ 
+------------------------ 
+2. Bahasa Indonesia 
+  - _Mencari 15 Pengertin Bahasa Menurut Para Ahli_ 
+------------------------ 
 *";
 ?>
