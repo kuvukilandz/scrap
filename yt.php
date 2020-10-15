9f2fd751aabd876892ecab5112f49cff2e8157f8
renzichwan <68050501+renzichwan@users.noreply.github.com>
2020-10-14 23:26:42 -0700

    Update yt.php

--- yt.php
+++ yt.php
@@ -4,11 +4,5 @@ $ch = curl_init('http://youtube-scrape.herokuapp.com/api/search?q='.$q.'&page=1'
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $upx = curl_exec($ch);
 $data = json_decode($upx,true);
- $url = $data['results'][0]['video']['url'];
-$cht = curl_init('http://scrap.terhambar.com/yt?link='.$url.'');
-curl_setopt($cht, CURLOPT_RETURNTRANSFER, true);
-$up = curl_exec($cht);
-$hasil = json_decode($up,true);
-$menit = $data['results'][0]['video']['duration'];
-echo $hasil['title']."@".$hasil['linkVideo']."@".$menit;
+ echo $data['results'][0]['video']['id']."^".$data['results'][0]['video']['title'];
 ?>
