2958c14539a98e8dcd5ec91619b47eab160cb3f1
renzichwan <68050501+renzichwan@users.noreply.github.com>
2020-11-13 08:40:59 -0800

    Update yt.php

--- yt.php
+++ yt.php
@@ -5,5 +5,10 @@ $ch = curl_init('http://youtube-scrape.herokuapp.com/api/search?q='.$q.'&page=1'
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $upx = curl_exec($ch);
 $data = json_decode($upx,true);
- echo $data['results'][0]['video']['id']."^".$data['results'][0]['video']['title']."^".$data['results'][0]['video']['thumbnail_src'];
+  $array = array(
+   'id' => $data['results'][0]['video']['id'],
+   'title' => $data['results'][0]['video']['title'],
+   'thumbnail' => $data['results'][0]['video']['thumbnail_src']
+   );
+print_r(json_decode($array, JSON_PRETTY_PRINT));
 ?>
