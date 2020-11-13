cf2e31d03e2e48fcb4b433bebc4f6594b6fb960d
renzichwan <68050501+renzichwan@users.noreply.github.com>
2020-11-13 08:35:55 -0800

    Update yt.php

--- yt.php
+++ yt.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 $q = $_GET['q'];
 $ch = curl_init('http://youtube-scrape.herokuapp.com/api/search?q='.$q.'&page=1');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
