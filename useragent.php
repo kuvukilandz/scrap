e7f733d7ba30a5ebb8969fca0e48219204f33941
renzichwan <68050501+renzichwan@users.noreply.github.com>
2021-01-16 22:56:54 +0800

    Update useragent.php

--- useragent.php
+++ useragent.php
@@ -1,4 +1,5 @@
 <?php
+header('Content-Type:appliaction/json');
 $ua = urlencode($_GET['ua']);
 $exec = 'http://api.userstack.com/detect?access_key=61a25c85e1d92c339a513bedf298918b&ua='.$ua;
 $ch = curl_init();
