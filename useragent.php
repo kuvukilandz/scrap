024559b673d97dc313290536055d98683e6d3e0d
renzichwan <68050501+renzichwan@users.noreply.github.com>
2021-01-16 22:51:01 +0800

    Create useragent.php

---
+++ useragent.php
@@ -0,0 +1,11 @@
+<?php
+$ua = urlencode($_GET['ua']);
+$exec = "https://api.userstack.com/detect?access_key=61a25c85e1d92c339a513bedf298918b&ua=$ua";
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,$exec);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
+curl_setopt($ch,CURLOPT_TIMEOUT, 20);
+$response = curl_exec($ch);
+print_r($response);
+?>
