56a5631af455e60013fef5a47a96d57841cd4b8c
renzichwan <68050501+renzichwan@users.noreply.github.com>
2021-01-16 23:03:37 +0800

    Update useragent.php

--- useragent.php
+++ useragent.php
@@ -7,7 +7,16 @@ curl_setopt($ch,CURLOPT_URL,$exec);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
 curl_setopt($ch,CURLOPT_TIMEOUT, 20);
-$response = curl_exec($ch);
-echo '<pre>';
-print_r($response);
+$res = curl_exec($ch);
+$data = json_decode($res, true);
+$nama = $data['name'];
+$os = $data['os']['name'];
+$browser = $data['browser']['name'];
+$hasil = array(
+  'device' => $nama,
+  'os' => $os,
+  'browser' => $browser
+  );
+$result = json_encode($hasil, JSON_PRETTY_PRINT);
+print_r($result);
 ?>
